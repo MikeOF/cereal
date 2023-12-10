4fd04e8cbc70f170f4689736104d00a5fa805a5b
mike-vantai <mike@vant.ai>
2023-12-09 21:47:39 -0500

    savepoint

--- src/cereal/protobufable.py
+++ src/cereal/protobufable.py
@@ -8,58 +8,60 @@ from typing import Optional
 
 
 class ProtoBufAble(ABC):
-    """Base blass that provides protobuf serialization."""
+    """Base class that provides protobuf serialization."""
 
-    PROTOBUF_DIRNAME = '__protobuf__'
-    PROTOFILE_SUFFIX = '.proto'
-    OPTIONAL_TYPE_SET = frozenset((str, bool, int, float))
-    REPEATED_TYPE_SET = frozenset((list, tuple, set))
-    OPTIONAL_TYPE_BY_TYPE = {str: 'string', bool: 'bool', int: 'int32', float: 'double'}
-    OPTIONAL_TYPE_BY_MEMBER: Optional[dict] = None
-    REPEATED_TYPE_BY_MEMBER: Optional[dict] = None
-    SOURCE_CLASS: Optional[type] = None
+    _PROTOBUF_DIRNAME = '__protobuf__'
+    _PROTOFILE_SUFFIX = '.proto'
+    _OPTIONAL_TYPE_BY_TYPE = {str: 'string', bool: 'bool', int: 'int32', float: 'double'}
+    _REPEATED_TYPE_SET = frozenset((list, tuple, set))
+    _OPTIONAL_TYPE_BY_MEMBER: Optional[dict] = None
+    _REPEATED_TYPE_BY_MEMBER: Optional[dict] = None
+    _MESSAGE_CLASS: Optional = None
 
     @classmethod
-    def get_protobuf_dir_path(cls) -> Path:
+    def _get_protobuf_dir_path(cls) -> Path:
         """Get the directory where protobuf files will be stored."""
-        return Path(inspect.getmodule(cls).__file__).parent.joinpath(cls.PROTOBUF_DIRNAME)
+        return Path(inspect.getmodule(cls).__file__).parent.joinpath(cls._PROTOBUF_DIRNAME)
 
     @classmethod
-    def get_proto_file_path(cls) -> Path:
+    def _get_protofile_path(cls) -> Path:
         """Get the proto file path for this class."""
-        return cls.get_protobuf_dir_path().joinpath(cls.__name__).with_suffix('.proto')
+        return cls._get_protobuf_dir_path().joinpath(cls.__name__).with_suffix('.proto')
 
     @classmethod
-    def get_source_file_path(cls) -> Path:
+    def _get_message_class_file_path(cls) -> Path:
         """Get the proto file path for this class."""
-        return cls.get_protobuf_dir_path().joinpath(f'{cls.__name__}_pb2.py')
+        return cls._get_protobuf_dir_path().joinpath(f'{cls.__name__}_pb2.py')
 
     @classmethod
-    def set_type_by_member_dicts(cls) -> None:
+    def _ensure_type_by_member_dicts(cls) -> None:
 
-        type_by_member = {
-            name: parameter.annotation
-            for name, parameter in inspect.signature(cls.__init__).parameters.items()
-            if name != 'self'
-        }
+        if cls._OPTIONAL_TYPE_BY_MEMBER is None or cls._REPEATED_TYPE_BY_MEMBER is None:
 
-        cls.OPTIONAL_TYPE_BY_MEMBER = {
-            name: member_type for name, member_type in type_by_member.items()
-            if member_type in cls.OPTIONAL_TYPE_BY_TYPE
-        }
+            type_by_member = {
+                name: parameter.annotation
+                for name, parameter in inspect.signature(cls.__init__).parameters.items()
+                if name != 'self'
+            }
 
-        cls.REPEATED_TYPE_BY_MEMBER = {
-            name: member_type for name, member_type in type_by_member.items()
-            if name not in cls.OPTIONAL_TYPE_BY_MEMBER
-        }
+            cls._OPTIONAL_TYPE_BY_MEMBER = {
+                name: member_type for name, member_type in type_by_member.items()
+                if member_type in cls._OPTIONAL_TYPE_BY_TYPE
+            }
+
+            cls._REPEATED_TYPE_BY_MEMBER = {
+                name: member_type for name, member_type in type_by_member.items()
+                if name not in cls._OPTIONAL_TYPE_BY_MEMBER
+            }
 
     @classmethod
-    def write_protofile(cls) -> None:
+    def _write_protofile(cls) -> None:
         """Write a proto file for this class."""
 
-        cls.get_protobuf_dir_path().mkdir(exist_ok=True)
+        # make the protobuf dir if necessary
+        cls._get_protobuf_dir_path().mkdir(exist_ok=True)
 
-        with cls.get_proto_file_path().open(mode='w') as outf:
+        with cls._get_protofile_path().open(mode='w') as outf:
 
             # write the header
             print('syntax = "proto3";', file=outf)
@@ -70,71 +72,108 @@ class ProtoBufAble(ABC):
             # write the message
             print(f'message {cls.__name__} {{', file=outf)
 
-            if cls.OPTIONAL_TYPE_BY_MEMBER is None or cls.REPEATED_TYPE_BY_MEMBER is None:
-                cls.set_type_by_member_dicts()
+            # set the type by member dicts if they are None
+            cls._ensure_type_by_member_dicts()
 
+            # write declarations for each repeated type
             element_count = 0
-            for member, repeated_type in cls.REPEATED_TYPE_BY_MEMBER.items():
+            for member, repeated_type in cls._REPEATED_TYPE_BY_MEMBER.items():
                 element_count += 1
-                assert isinstance(repeated_type, GenericAlias)
+                assert isinstance(repeated_type, GenericAlias), f'unexpected repeated type, {member} {repeated_type}'
                 container_type = getattr(repeated_type, '__origin__')
                 element_type = next(iter(getattr(repeated_type, '__args__')))
-                assert container_type in cls.REPEATED_TYPE_SET
-                assert element_type in cls.OPTIONAL_TYPE_SET
-                print(f'\trepeated {cls.OPTIONAL_TYPE_BY_TYPE[element_type]} {member} = {element_count};', file=outf)
+                assert container_type in cls._REPEATED_TYPE_SET, f'unaccepted container type, {member} {container_type}'
+                assert element_type in cls._OPTIONAL_TYPE_BY_TYPE, f'unaccepted type, {member} {element_type}'
+                print(
+                    f'\trepeated {cls._OPTIONAL_TYPE_BY_TYPE[element_type]} {member} = {element_count};',
+                    file=outf
+                )
                 print(f'', file=outf)
 
-            for member, optional_type in cls.OPTIONAL_TYPE_BY_MEMBER.items():
+            # write declarations for each optional type
+            for member, optional_type in cls._OPTIONAL_TYPE_BY_MEMBER.items():
                 element_count += 1
-                print(f'\toptional {cls.OPTIONAL_TYPE_BY_TYPE[optional_type]} {member} = {element_count};', file=outf)
+                print(
+                    f'\toptional {cls._OPTIONAL_TYPE_BY_TYPE[optional_type]} {member} = {element_count};',
+                    file=outf
+                )
                 print(f'', file=outf)
 
             print('}', file=outf)
 
     @classmethod
-    def compile(cls) -> None:
+    def _compile(cls) -> None:
 
-        cmd_tuple = [
-            'protoc',
-            f'-I={cls.get_protobuf_dir_path()}',
-            f'--python_out={cls.get_protobuf_dir_path()}',
-            f'{cls.get_proto_file_path()}'
-        ]
+        # get the protofile
+        protofile_path = cls._get_protofile_path()
 
+        # write the protofile if it doesn't exist
+        if not protofile_path.is_file():
+            cls._write_protofile()
+
+        # run the compile command
+        protobuf_dir_path = cls._get_protobuf_dir_path()
+        cmd_tuple = ('protoc', f'-I={protobuf_dir_path}', f'--python_out={protobuf_dir_path}', f'{protofile_path}')
         subprocess.run(cmd_tuple)
 
-        assert cls.get_source_file_path().exists(), (
-            f'could not find source path, {list(cls.get_protobuf_dir_path().glob("*"))}'
+        # make sure the message class file can be found
+        assert cls._get_message_class_file_path().exists(), (
+            f'could not find message class path, {list(cls._get_protobuf_dir_path().glob("*"))}'
         )
 
     @classmethod
-    def prepare_proto(cls) -> None:
-        cls.write_protofile()
-        cls.compile()
+    def _import_message_class(cls) -> None:
 
-    @classmethod
-    def import_source_class(cls) -> None:
+        if cls._MESSAGE_CLASS is None:
 
-        if cls.SOURCE_CLASS is None:
+            # get the message class file path and make sure it exists
+            message_class_file_path = cls._get_message_class_file_path()
+            if not message_class_file_path.exists():
+                cls._compile()
 
-            # load the source module
-            source_file_path = cls.get_source_file_path()
-            loader = SourceFileLoader(fullname=source_file_path.stem, path=str(source_file_path))
-            source_module = loader.load_module()
+            # load the mesage module
+            loader = SourceFileLoader(fullname=message_class_file_path.stem, path=str(message_class_file_path))
+            message_module = loader.load_module()
 
             # get the source class
-            cls.SOURCE_CLASS = getattr(source_module, cls.__name__)
+            cls._MESSAGE_CLASS = getattr(message_module, cls.__name__)
 
-    def to_protobuf(self) -> str:
+    def to_protobuf(self) -> bytes:
 
-        if self.SOURCE_CLASS is None:
-            self.import_source_class()
+        # import the message class if necessary
+        if self._MESSAGE_CLASS is None:
+            self._import_message_class()
 
-        instance = self.SOURCE_CLASS()
-        for member in self.OPTIONAL_TYPE_BY_MEMBER:
+        # set the type by member dicts if they are None
+        self._ensure_type_by_member_dicts()
+
+        # create an instance of the message class
+        instance = self._MESSAGE_CLASS()
+
+        # add each optional member
+        for member in self._OPTIONAL_TYPE_BY_MEMBER:
             setattr(instance, member, getattr(self, member))
 
-        for member in self.REPEATED_TYPE_BY_MEMBER:
+        # add each repeated member
+        for member in self._REPEATED_TYPE_BY_MEMBER:
             getattr(instance, member).extend(getattr(self, member))
 
+        # serialize and return
         return instance.SerializeToString()
+
+    @classmethod
+    def from_protobuf(cls, protobuf: bytes):
+
+        # import the message class if necessary
+        if cls._MESSAGE_CLASS is None:
+            cls._import_message_class()
+
+        # create the message instance
+        instance = cls._MESSAGE_CLASS()
+        instance.ParseFromString(protobuf)
+
+        # create the constructor arguments
+        kwarg_dict = {member: getattr(instance, member) for member in cls._OPTIONAL_TYPE_BY_MEMBER}
+        kwarg_dict.update((member, getattr(instance, member)) for member in cls._REPEATED_TYPE_BY_MEMBER)
+
+        return cls(**kwarg_dict)
--- tst/scripts/crl-test-protobuf.py
+++ tst/scripts/crl-test-protobuf.py
@@ -9,12 +9,17 @@ from tester.simple import Simple
 def main():
 
     # write the protofile
-    Simple.write_protofile()
-    Simple.compile()
+    Simple._write_protofile()
+    Simple._compile()
 
     # create an instance of the class
     simple = Simple(a=5, b=True, c=[5.5, 6.6, 7.7])
-    print(simple.to_protobuf())
+    protobuf_str = simple.to_protobuf()
+    print(simple.__dict__)
+    print(type(protobuf_str))
+    print(protobuf_str)
+    simple_from = Simple.from_protobuf(protobuf=protobuf_str)
+    print(simple_from.__dict__)
 
 
 if __name__ == '__main__':
