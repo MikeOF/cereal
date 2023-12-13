979c8382acc68558552d1f1f42ab8b175b1f8787
mike-vantai <mike@vant.ai>
2023-12-13 16:47:37 -0500

    savepoint

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+extra_requirement_list_by_name = {
+    'protobuf': ['protobuf'],
+    'bson': ['bson']
+}
+
 setup(
     name='cereal',
     version='0.1.0',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    requires=['protobuf']
+    extras_require=extra_requirement_list_by_name
 )
--- src/cereal/jsonable.py
+++ src/cereal/jsonable.py
@@ -1,7 +1,8 @@
 import inspect
 import json
 from abc import ABCMeta
-from typing import Optional
+from pathlib import Path
+from typing import Optional, Union
 
 
 class JSONAble(metaclass=ABCMeta):
@@ -19,8 +20,13 @@ class JSONAble(metaclass=ABCMeta):
             }
 
     @classmethod
-    def from_json_str(cls, json_str):
-        return cls.from_json(json_obj=json.loads(json_str))
+    def from_json_file(cls, file_path: Union[Path, str]):
+        with Path(file_path).open(mode='r') as inf:
+            return cls.from_json_str(json_str=inf.read())
+
+    @classmethod
+    def from_json_str(cls, json_str: str):
+        return cls.from_json(json_obj=cls._SERIALIZATION_MODULE.loads(json_str))
 
     @classmethod
     def from_json(cls, json_obj: dict):
@@ -28,10 +34,14 @@ class JSONAble(metaclass=ABCMeta):
         cls._ensure_constructor_parameter_set()
         construction_kwarg_by_name = {k: v for k, v in json_obj.items() if k in cls._CONSTRUCTOR_PARAMETER_SET}
 
-        return  cls(**construction_kwarg_by_name)
+        return cls(**construction_kwarg_by_name)
+
+    def to_json_file(self, file_path: Union[Path, str], indent: int = None) -> None:
+        with Path(file_path).open(mode='w') as outf:
+            outf.write(self.to_json_str(indent=indent))
 
     def to_json_str(self, indent: int = None):
-        return json.dumps(self.to_json(), indent=indent)
+        return self._SERIALIZATION_MODULE.dumps(self.to_json(), indent=indent)
 
     def to_json(self):
         self._ensure_constructor_parameter_set()
