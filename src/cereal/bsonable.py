11122d11adeea67b688c6e5f90d1c3295c2ce4cc
mike-vantai <mike@vant.ai>
2023-12-14 09:01:29 -0500

    savepoint

--- src/cereal/bsonable.py
+++ src/cereal/bsonable.py
@@ -1,10 +1,43 @@
+import inspect
 from abc import ABCMeta
+from pathlib import Path
+from typing import Optional, Union
 
 import bson
 
-from cereal.jsonable import JSONAble
 
+class BSONAble(metaclass=ABCMeta):
 
-class BSONAble(JSONAble, metaclass=ABCMeta):
+    _CONSTRUCTOR_PARAMETER_SET: Optional[set[str]] = None
 
-    _SERIALIZATION_MODULE = bson
+    @classmethod
+    def _ensure_constructor_parameter_set(cls) -> None:
+
+        if cls._CONSTRUCTOR_PARAMETER_SET is None:
+
+            cls._CONSTRUCTOR_PARAMETER_SET = {
+                name for name in inspect.signature(cls.__init__).parameters if name != 'self'
+            }
+
+    @classmethod
+    def from_bson_file(cls, file_path: Union[Path, str]):
+        with Path(file_path).open(mode='rb') as inf:
+            return cls.from_bson(bson_bytes=inf.read())
+
+    @classmethod
+    def from_bson(cls, bson_bytes: bytes):
+
+        instance_dict = bson.loads(data=bson_bytes)
+
+        cls._ensure_constructor_parameter_set()
+        construction_kwarg_by_name = {k: v for k, v in instance_dict.items() if k in cls._CONSTRUCTOR_PARAMETER_SET}
+
+        return cls(**construction_kwarg_by_name)
+
+    def to_bson_file(self, file_path: Union[Path, str]) -> None:
+        with Path(file_path).open(mode='wb') as outf:
+            outf.write(self.to_bson())
+
+    def to_bson(self) -> bytes:
+        self._ensure_constructor_parameter_set()
+        return bson.dumps(obj={k: getattr(self, k) for k in self._CONSTRUCTOR_PARAMETER_SET})
--- src/cereal/jsonable.py
+++ src/cereal/jsonable.py
@@ -7,7 +7,6 @@ from typing import Optional, Union
 
 class JSONAble(metaclass=ABCMeta):
 
-    _SERIALIZATION_MODULE = json
     _CONSTRUCTOR_PARAMETER_SET: Optional[set[str]] = None
 
     @classmethod
@@ -26,7 +25,7 @@ class JSONAble(metaclass=ABCMeta):
 
     @classmethod
     def from_json_str(cls, json_str: str):
-        return cls.from_json(json_obj=cls._SERIALIZATION_MODULE.loads(json_str))
+        return cls.from_json(json_obj=json.loads(json_str))
 
     @classmethod
     def from_json(cls, json_obj: dict):
@@ -40,9 +39,9 @@ class JSONAble(metaclass=ABCMeta):
         with Path(file_path).open(mode='w') as outf:
             outf.write(self.to_json_str(indent=indent))
 
-    def to_json_str(self, indent: int = None):
-        return self._SERIALIZATION_MODULE.dumps(self.to_json(), indent=indent)
+    def to_json_str(self, indent: int = None) -> str:
+        return json.dumps(self.to_json(), indent=indent)
 
-    def to_json(self):
+    def to_json(self) -> dict:
         self._ensure_constructor_parameter_set()
         return {k: getattr(self, k) for k in self._CONSTRUCTOR_PARAMETER_SET}
