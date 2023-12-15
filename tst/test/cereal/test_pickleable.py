fdc18d4ec7cb143d8a4003f5476554aaac36da1f
mike-vantai <mike@vant.ai>
2023-12-14 21:38:08 -0500

    savepoint

--- tst/test/cereal/test_pickleable.py
+++ tst/test/cereal/test_pickleable.py
@@ -1,6 +1,8 @@
 import tempfile
 from pathlib import Path
 
+import pytest
+
 from cereal.pickleable import PickleAble
 
 
@@ -17,17 +19,31 @@ class Basic(PickleAble):
         return ' '.join(map(str, (self.a, self.c, self.b, self.e)))
 
 
-def test_roundtrip():
-
-    # create the test instance
+# define fixtures
+@pytest.fixture()
+def test_instance() -> Basic:
     instance = Basic(a=1, b=['hello', 'there'], c={'I': [5.5, 6.6, 7.7], 'am': [1.1, 2.2, 3.3], 'a': [4.4, 5.5]})
     instance.e = ['a', 'b', 'c']
+    return instance
+
+
+def test_roundtrip_file(test_instance):
 
     # roundtrip the instance
     temp_file_path = Path(tempfile.mkdtemp()).joinpath('file.pkl')
-    instance.to_pickle_file(file_path=temp_file_path)
+    test_instance.to_pickle_file(file_path=temp_file_path)
     instance_roundtrip = Basic.from_pickle_file(file_path=temp_file_path)
 
     # check
-    assert instance.__dict__ == instance_roundtrip.__dict__
+    assert test_instance.__dict__ == instance_roundtrip.__dict__
+
+
+def test_roundtrip_bytes(test_instance):
+
+    # roundtrip the instance
+    obj_bytes = test_instance.to_pickle_bytes()
+    instance_roundtrip = Basic.from_pickle_bytes(obj_bytes=obj_bytes)
+
+    # check
+    assert test_instance.__dict__ == instance_roundtrip.__dict__
 
