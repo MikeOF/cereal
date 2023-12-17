ed974994bb3a7ca97c5a356e594cbdf51320a3ac
mike-vantai <mike@vant.ai>
2023-12-17 16:44:28 -0500

    savepoint

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -33,8 +33,7 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,7 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
