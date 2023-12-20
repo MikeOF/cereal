a31a1a4fe1a8bf8cdfae762eb25101b34bcdc557
mike-vantai <mike@vant.ai>
2023-12-19 23:36:36 -0500

    savepoint

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
-extra_requirement_list_by_name = {
-    'protobuf': ['protobuf'],
-    'bson': ['bson']
-}
 
 setup(
     name='piquid',
     version='0.1.0',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    extras_require=extra_requirement_list_by_name
+    install_requires=(
+        'bson',
+        'protobuf'
+    )
 )
